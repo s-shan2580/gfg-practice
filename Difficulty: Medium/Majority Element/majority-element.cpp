6030798626ed39434bca2251c6a4c98623202df6
S_Shan <ushatiwary108@gmail.com>
2026-07-31 20:45:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int majorityElement(vector<int>& nums) {
+        // code here
+         unordered_map<int,int>mp;
+        for(int x: nums){
+            mp[x]++;
+        }
+        for(auto it: mp){
+            if(it.second>(nums.size()/2)){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
