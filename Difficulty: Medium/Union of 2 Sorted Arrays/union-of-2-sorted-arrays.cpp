8820854d9070dc4aa75d9e26545540d8a3a80d14
S_Shan <ushatiwary108@gmail.com>
2026-08-10 15:40:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
@@ -1,51 +1,40 @@
 class Solution {
-	public:
-	vector<int> findUnion(vector<int> &a, vector<int> &b) {
-		// code here
-		int n1 = a.size();
-		int n2 = b.size();
-		
-		vector<int> res;
-		
-		int i = 0;
-		int j = 0;
-		
-		while (i<n1 && j<n2) {
-			if (a[i] <= b[j]) {
-				if (res.size() == 0 || res.back() != a[i]) {
-					res.push_back(a[i]);
-					
-				}
-				i++;
-			}
-			else{
-			   if (res.size() == 0 || res.back() != b[j]) {
-					res.push_back( b[j]);
-					
-				}
-				j++; 
-			}
-			
-		}
-		
-		while(i<n1){
-		    	if (res.size() == 0 || res.back() != a[i]) {
-					res.push_back(a[i]);
-					
-				}
-				i++;
-		}
-		
-		while(j<n2){
-		    	if (res.size() == 0 || res.back() != b[j]) {
-					res.push_back(b[j]);
-					
-				}
-				j++;
-		}
-		
-		return res;
-		
-		
-	}
+  public:
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // code here
+        int n1=a.size();
+        int n2=b.size();
+        
+        vector<int> res;
+        
+        int i=0;
+        int j=0;
+        
+        while(i<n1 && j<n2){
+           if(a[i]<b[j]){
+               if(res.empty() || res.back()!=a[i]) res.push_back(a[i]);
+               i++;
+           }
+           else if(a[i]>b[j]){
+                if(res.empty() || res.back()!=b[j]) res.push_back(b[j]);
+               j++;
+           }
+           else{
+               if(res.empty() || res.back()!=b[j]) res.push_back(b[j]);
+               i++; j++;
+           }
+        }
+        
+        while(i<n1){
+            if(res.empty() || res.back()!=a[i]) res.push_back(a[i]);
+               i++;
+        }
+        
+        while(j<n2){
+            if(res.empty() || res.back()!=b[j]) res.push_back(b[j]);
+            j++;
+        }
+        
+        return res;
+    }
 };
